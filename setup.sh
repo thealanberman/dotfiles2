b585a23ace0b8b585522ff3fc0797242d2b75f3b
Al <thealanberman@gmail.com>
2016-11-18 22:48:05 -0800

    todo list

--- setup.sh
+++ setup.sh
@@ -5,3 +5,10 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ -f ${THIS_DIR}/.bash_profile ]]; then
   ln -v -s ${THIS_DIR}/.bash_profile ${HOME}
 fi
+
+# TODO:
+# Customize .inputrc
+# Install homebrew
+# Install homebrew apps
+# Install cask apps
+# Symlink other dotfiles
